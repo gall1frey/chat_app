b0fce8d754926867c38ba472d3eed45d521b2f95
Gallifrey <m>
2021-03-25 03:11:14 +0530

    Chatroom works, need to add Name checking and colours to output.

--- client/client.c
+++ client/client.c
@@ -1,44 +1,132 @@
 #include <stdio.h>
-#include <sys/socket.h> //For Sockets
 #include <stdlib.h>
-#include <netinet/in.h> //For the AF_INET (Address Family)
-#include <sys/types.h>
 #include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <pthread.h>
 
+#define LEN 2048
+
+volatile sig_atomic_t flag = 0;
 struct sockaddr_in serv; //This is our main socket variable.
 int fd; //This is the socket file descriptor that will be used to identify the socket
 int conn; //This is the connection file descriptor that will be used to distinguish client connections.
-char message[100] = ""; //This array will store the messages that are sent by the server
+//char message[LEN-32] = ""; //This array will store the messages that are sent by the server
+char name[32]; //This array will store the client's name
 
-void send_func() {
-  while(1) {
-    printf("Enter a message: ");
-    fgets(message, 100, stdin);
-    send(fd, message, strlen(message), 0);
+void str_overwrite_stdout() {
+  printf("%s", "> ");
+  fflush(stdout);
+}
+
+void str_trim_lf (char* arr, int length) {
+  int i;
+  for (i = 0; i < length; i++) { // trim \n
+    if (arr[i] == '\n') {
+      arr[i] = '\0';
+      break;
+    }
   }
 }
 
-void* recv_func(void *arg) {
-  char msg[100];
-  printf("Thread starting...\n");
-  while (recv(fd, msg, 100, 0)>0) {
-    printf("Message Received: %s\n", msg);
-    strcpy(msg,"\0");
+void catch_ctrl_c_and_exit(int sig) {
+    flag = 1;
+}
+
+void send_func() {
+  char message[LEN] = {};
+	char buffer[LEN + 32] = {};
+  while(1) {
+    str_overwrite_stdout();
+    fgets(message, 100, stdin);
+    str_trim_lf(message,LEN);
+    if(!strcmp(message,"exit"))
+      break;
+    else{
+      sprintf(buffer,"%s: %s\n", name, message);
+      send(fd, buffer, strlen(buffer), 0);
+    }
+    bzero(message,LEN);
+    bzero(buffer,LEN+32);
   }
-  return NULL;
+  catch_ctrl_c_and_exit(2);
+}
+
+void recv_func() {
+  char message[LEN] = {};
+  //printf("Thread starting...\n");
+  while (1) {
+    int receive = recv(fd, message, LEN, 0);
+    if (receive > 0) {
+      printf("%s\n", message);
+      str_overwrite_stdout();
+    } else if (receive == 0) {
+      break;
+    } else {
+      catch_ctrl_c_and_exit(2);
+    }
+    memset(message,0,sizeof(message));
+  }
 }
 
 int main(int argc, char const *argv[]) {
+  signal(SIGINT, catch_ctrl_c_and_exit);
+
   fd = socket(AF_INET, SOCK_STREAM, 0);
   serv.sin_family = AF_INET;
   serv.sin_port = htons(8096);
   inet_pton(AF_INET, "127.0.0.1", &serv.sin_addr); //This binds the client to localhost
   connect(fd, (struct sockaddr *)&serv, sizeof(serv)); //This connects the client to the server.
-  pthread_t thread;
-  pthread_create(&thread,NULL,recv_func,NULL);
-  send_func();
-    //An extra breaking condition can be added here (to terminate the while loop)
-  return 0;
+
+  //Set name
+  while (1) {
+    printf("Please enter your name: ");
+    fgets(name, 32, stdin);
+    str_trim_lf(name, strlen(name));
+    //Name validation
+    if (strlen(name) > 32 || strlen(name) < 2){
+  		printf("Name must be less than 30 and more than 2 characters.\n");
+  		return EXIT_FAILURE;
+  	}
+    char buf[32] = {};
+    sprintf(buf,"NM>%s",name);
+    printf("[DEBUG]: Sending: %s\n",buf);
+    send(fd, buf, 32, 0);
+    int receive = recv(fd, buf, 3, 0);
+    printf("[DEBUG]: Received: %s\n",buf);
+    if (receive > 0 && !strcmp(buf,"OK")){
+      break;
+    }
+    else
+      printf("Name already exists in chatroom. Try again.\n");
+  }
+
+  printf("WELCOME TO CHATROOM!\n");
+
+  pthread_t send_msg_thread;
+  if(pthread_create(&send_msg_thread, NULL, (void *) send_func, NULL) != 0){
+		printf("ERROR: pthread\n");
+    return EXIT_FAILURE;
+	}
+
+	pthread_t recv_msg_thread;
+  if(pthread_create(&recv_msg_thread, NULL, (void *) recv_func, NULL) != 0){
+		printf("ERROR: pthread\n");
+		return EXIT_FAILURE;
+	}
+
+  while (1){
+    if(flag){
+      printf("\nBye\n");
+      break;
+    }
+  }
+
+  close(fd);
+
+  return EXIT_SUCCESS;
 }
--- server/server.c
+++ server/server.c
@@ -1,37 +1,197 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdio.h>
-#include <sys/socket.h> //For Sockets
 #include <stdlib.h>
-#include <netinet/in.h> //For the AF_INET (Address Family)
-#include <sys/types.h>
 #include <unistd.h>
+#include <errno.h>
 #include <string.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <signal.h>
 
-struct sockaddr_in serv; //This is our main socket variable.
-int fd; //This is the socket file descriptor that will be used to identify the socket
-int conn; //This is the connection file descriptor that will be used to distinguish client connections.
-char message[100]; //This array will store the messages that are sent by the server
-int sockets[5];
+#define MAX_CLIENTS 5
+#define LEN 2080
 
-int main(int argc, char const *argv[]) {
-  serv.sin_family = AF_INET;
-  serv.sin_port = htons(8096); //Define the port at which the server will listen for connections.
-  serv.sin_addr.s_addr = INADDR_ANY;
-  fd = socket(AF_INET, SOCK_STREAM, 0); //This will create a new socket and also return the identifier of the socket into fd.
-  // To handle errors, you can add an if condition that checks whether fd is greater than 0. If it isn't, prompt an error
-  bind(fd, (struct sockaddr *)&serv, sizeof(serv)); //assigns the address specified by serv to the socket
-  listen(fd,5); //Listen for client connections. Maximum 5 connections will be permitted.
-  //Now we start handling the connections.
-  while(conn = accept(fd, (struct sockaddr *)NULL, NULL)) {
-    int pid;
-    if((pid = fork()) == 0) {
-        while (recv(conn, message, 100, 0)>0) {
-            printf("Message Received: %s\n", message);
-            send(conn,message,strlen(message),0);
-            //An extra breaking condition can be added here (to terminate the child process)
-            strcpy(message,"\0");
-        }
-        exit(0);
+static _Atomic unsigned int cli_count = 0;
+static int uid = 10;
+
+typedef struct{
+	struct sockaddr_in address;
+	int sockfd;
+	int uid;
+	char name[32];
+  char color[32];
+} CLIENT;
+
+CLIENT *clients[MAX_CLIENTS];
+pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void str_overwrite_stdout() {
+    printf("\r%s", "> ");
+    fflush(stdout);
+}
+
+void str_trim_lf (char* arr, int length) {
+  int i;
+  for (i = 0; i < length; i++) { // trim \n
+    if (arr[i] == '\n') {
+      arr[i] = '\0';
+      break;
     }
   }
-  return 0;
+}
+
+void queue_add(CLIENT *cl){
+	pthread_mutex_lock(&clients_mutex);
+	for(int i=0; i < MAX_CLIENTS; ++i){
+		if(!clients[i]){
+			clients[i] = cl;
+			break;
+		}
+	}
+	pthread_mutex_unlock(&clients_mutex);
+}
+
+void queue_remove(int uid){
+	pthread_mutex_lock(&clients_mutex);
+
+	for(int i=0; i < MAX_CLIENTS; ++i){
+		if(clients[i]){
+			if(clients[i]->uid == uid){
+				clients[i] = NULL;
+				break;
+			}
+		}
+	}
+
+	pthread_mutex_unlock(&clients_mutex);
+}
+
+void send_msg(char *msg, int uid){
+	pthread_mutex_lock(&clients_mutex);
+	for(int i=0; i<MAX_CLIENTS; i++){
+		if(clients[i] && clients[i]->uid != uid){
+			if(write(clients[i]->sockfd,msg,strlen(msg)) < 0){
+				perror("[ERROR]: Write to descriptor failed.");
+				break;
+			}
+		}
+	}
+	pthread_mutex_unlock(&clients_mutex);
+}
+
+void *handle_client(void *c_id){
+	char name_msg[32] = {};
+	char message_buf[LEN];
+	int leave_flag = 0;
+	cli_count++;
+	CLIENT *cli = (CLIENT *)c_id;
+
+	int receive = recv(cli->sockfd, name_msg, LEN, 0);
+	if (receive > 0 && name_msg[0] == 'N' && name_msg[1] == 'M' && name_msg[2] == '>'){
+		int i = 0;
+		while(name_msg[i] != '\0' && i < 32){
+			cli->name[i] = name_msg[i+3];
+			i++;
+		}
+		sprintf(message_buf, "%s has joined\n", cli->name);
+		printf("%s\n", message_buf);
+		send(cli->sockfd,"OK",3,0);
+		send_msg(message_buf,cli->uid);
+	} else {
+		leave_flag = 1;
+	}
+	bzero(message_buf, LEN);
+	while(1){
+		if(leave_flag) break;
+
+		int receive = recv(cli->sockfd, message_buf, LEN, 0);
+		if(receive > 0){
+			if( strlen(message_buf) > 0 ){
+
+				send_msg(message_buf, cli->uid);
+				str_trim_lf(message_buf, strlen(message_buf));
+				printf("%s -> %s\n",cli->name,message_buf);
+			}
+		} else if (receive == 0 || !strcmp(message_buf, "exit")){
+			sprintf(message_buf, "%s has left.\n", cli->name);
+			printf("%s", message_buf);
+			send_msg(message_buf, cli->uid);
+			leave_flag = 1;
+		} else {
+			printf("[ERROR]: -1\n");
+			leave_flag = 1;
+		}
+		bzero(message_buf, LEN);
+	}
+	close(cli->sockfd);
+	queue_remove(cli->uid);
+	free(cli);
+	cli_count--;
+	pthread_detach(pthread_self());
+	return NULL;
+}
+
+int main(int argc, char const *argv[]) {
+	char *ip = "127.0.0.1";
+	int port = 8096;
+	int option = 1, listenfd = 1, connfd = 0;
+
+	struct sockaddr_in serv_addr;
+	struct sockaddr_in cli_addr;
+	pthread_t tid;
+
+	signal(SIGPIPE, SIG_IGN);
+
+	listenfd = socket(AF_INET, SOCK_STREAM, 0); //This will create a new socket and also return the identifier of the socket into fd.
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(port); //Define the port at which the server will listen for connections.
+  serv_addr.sin_addr.s_addr = inet_addr(ip);
+
+	if(setsockopt(listenfd, SOL_SOCKET,(SO_REUSEPORT | SO_REUSEADDR),(char*)&option,sizeof(option)) < 0){
+		perror("ERROR: setsockopt failed");
+    return EXIT_FAILURE;
+	}
+
+	/* Bind */
+  if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+    perror("ERROR: Socket binding failed");
+    return EXIT_FAILURE;
+  }
+
+	//Listen for client connections. Maximum 5 connections will be permitted.
+	if (listen(listenfd, 10) < 0) {
+		perror("ERROR: Socket listening failed");
+		return EXIT_FAILURE;
+	}
+
+	printf("CHATROOM SERVER\n");
+
+	while (1) {
+		socklen_t clilen = sizeof(cli_addr);
+		connfd = accept(listenfd, (struct sockaddr*)&cli_addr, &clilen);
+		if((cli_count + 1) == MAX_CLIENTS){
+			printf("Max clients reached. Rejected: ");
+			//print_client_addr(cli_addr);
+			printf(":%d\n", cli_addr.sin_port);
+			close(connfd);
+			continue;
+		}
+
+		/* Client settings */
+		CLIENT *cli = (CLIENT *)malloc(sizeof(CLIENT));
+		cli->address = cli_addr;
+		cli->sockfd = connfd;
+		cli->uid = uid++;
+
+		/* Add client to the queue and fork thread */
+		queue_add(cli);
+		pthread_create(&tid, NULL, &handle_client, (void*)cli);
+
+		/* Reduce CPU usage */
+		sleep(1);
+	}
+
+	return EXIT_SUCCESS;
 }
